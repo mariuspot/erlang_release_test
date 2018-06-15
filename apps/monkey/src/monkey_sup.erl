f00d351d01e6bd4509d1efd738e83f3596195348
Marius Potgieter <mariuspotgieter@gmail.com>
2018-06-15 09:35:04 +0200

    fixed child spec

--- apps/monkey/src/monkey_sup.erl
+++ apps/monkey/src/monkey_sup.erl
@@ -28,7 +28,7 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, [monkey_gen, {monkey_gen, start_link, []}, permanent, 1000, worker, [monkey_gen]]} }.
+    {ok, { {one_for_all, 0, 1}, [{monkey_gen, {monkey_gen, start_link, []}, permanent, 1000, worker, [monkey_gen]}]} }.
 	% {ok, { {one_for_all, 0, 1}, [{client_smpp_server,
     %         {client_smpp_server, start_link, ["client_smpp_server", self()]},
     %         permanent, 1000, worker, [client_smpp_server]}]} }.
